aa2443faed136cb79c392dda57a910608d5c6ef3
Jeffrey Liu <jeffkliu@gmail.com>
2020-03-05 23:36:45 -0500

    recfactor onSearchSubmit to arrow function and async and await

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,9 @@ import SearchBar from './SearchBar';
 
 class App extends React.Component {
 
-   async onSearchSubmit(term){
+   state = { images: [] };
+
+    onSearchSubmit = async (term) => { 
       console.log(term);
       const response = await axios.get('https://api.unsplash.com/search/photos', {
          params: { query: term },
@@ -12,14 +14,15 @@ class App extends React.Component {
             Authorization: 'Client-ID uPdl-jlL3nPNM_5oEzLlWOWwHZNKnNI7XlrzcUE3LGw'
          }
       });
-      
-      console.log(response.data.results); 
+
+      this.setState({ images: response.data.results });
    }
 
    render(){
       return (
          <div className="ui container" style={{marginTop: '10px '}}>
             <SearchBar onSubmit={this.onSearchSubmit } />
+            Found: {this.state.images.length} images
          </div>
       );
    }

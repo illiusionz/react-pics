4b6eeec061134ad64a1d665e576d42c14598dc63
Jeffrey Liu <jeffkliu@gmail.com>
2020-03-08 23:52:32 -0400

    refactor image map array

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const ImageList = (props) => {
 
-  const images = props.images.map((image) => {
-      return <img key={image.id} src={image.urls.regular} alt={image.description} />
+  const images = props.images.map(({ description, id, urls }) => {
+      return <img alt={description} key={id} src={urls.regular} />
    });
    
    return <div>{images}</div>
